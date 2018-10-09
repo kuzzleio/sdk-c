d30063ad36ebf23ad4b39994b6f062a9978fa446
Travis CI <support@kuzzle.io>
2018-10-09 16:32:34 +0200

    Add submodule alignment before build

--- .ci/align-submodules-on-merge.sh
+++ .ci/align-submodules-on-merge.sh
@@ -6,11 +6,11 @@ if [[ "$TRAVIS_BRANCH" == *"-dev" ]] || [ "$TRAVIS_BRANCH" = "master" ]; then
 
   git config --global user.email "support@kuzzle.io"
   git config --global user.name "Travis CI"
-
-  git clone --quiet --branch=${TRAVIS_BRANCH} https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} travis-repository-copy > /dev/null 2>&1
+  echo "clone"
+  git clone --quiet --branch=${TRAVIS_BRANCH} https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} travis-repository-copy > /dev/null
 
   cd travis-repository-copy
-  ./align-submodules
+  ./align-submodules.sh
 
   git commit -am "Travis CI - [ci skip] - align submodules to $TRAVIS_BRANCH" > /dev/null 2>&1
   git push origin ${TRAVIS_BRANCH}
