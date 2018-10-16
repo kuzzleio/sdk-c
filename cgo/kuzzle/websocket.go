9a0d87b33b68ac1f2b6c1e0db25da36cab2eb27f
jenow <jenova.ke20@gmail.com>
2018-10-16 19:28:42 +0200

    init websocket implementation

---
+++ cgo/kuzzle/websocket.go
@@ -0,0 +1,37 @@
+package main
+
+/*
+	#cgo CFLAGS: -I../../include
+	#include <stdlib.h>
+	#include "kuzzlesdk.h"
+	#include "protocol.h"
+
+	typedef struct {
+		void* instance;
+	} web_socket;
+*/
+import "C"
+import (
+	"unsafe"
+	"sync"
+	"github.com/kuzzleio/sdk-go/protocol/websocket"
+)
+
+// map which stores instances to keep references in case the gc passes
+var webSocketInstances sync.Map
+
+// register new instance to the instances map
+func registerWebSocket(instance interface{}, ptr unsafe.Pointer) {
+	webSocketInstances.Store(instance, ptr)
+}
+
+// export kuzzle_new_websocket
+func kuzzle_new_websocket(ws *C.web_socket, host *C.char, options *C.options) {
+	inst := websocket.NewWebSocket(C.GoString(host), SetOptions(options))
+
+	ptr := unsafe.Pointer(&inst)
+	ws.instance = ptr
+
+	registerWebSocket(inst, ptr)
+}
+
