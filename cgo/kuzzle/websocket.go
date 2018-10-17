29c158ce765e718379064d9b89e4ad52a35e7946
jenow <jenova.ke20@gmail.com>
2018-10-17 09:21:01 +0200

    wip

--- cgo/kuzzle/websocket.go
+++ cgo/kuzzle/websocket.go
@@ -5,6 +5,10 @@ package main
 	#include <stdlib.h>
 	#include "kuzzlesdk.h"
 	#include "protocol.h"
+
+	static void bridge_trigger_event_listener(kuzzle_event_listener listener, int event, char* res, void* data) {
+		listener(event, res, data);
+	}
 */
 import "C"
 import (
@@ -21,8 +25,8 @@ func registerWebSocket(instance interface{}, ptr unsafe.Pointer) {
 	webSocketInstances.Store(instance, ptr)
 }
 
-//export kuzzle_new_web_socket
-func kuzzle_new_web_socket(ws *C.web_socket, host *C.char, options *C.options) {
+//export kuzzle_websocket_new_web_socket
+func kuzzle_websocket_new_web_socket(ws *C.web_socket, host *C.char, options *C.options) {
 	inst := websocket.NewWebSocket(C.GoString(host), SetOptions(options))
 
 	ptr := unsafe.Pointer(&inst)
@@ -31,3 +35,18 @@ func kuzzle_new_web_socket(ws *C.web_socket, host *C.char, options *C.options) {
 	registerWebSocket(inst, ptr)
 }
 
+//export kuzzle_websocket_add_listener
+func kuzzle_websocket_add_listener(ws *C.web_socket, event C.int, listener C.kuzzle_event_listener) {
+	channel := make(chan interface{})
+	go func() {
+		for {
+			res, ok := <-channel
+			if ok == false {
+				break
+			}
+
+			C.bridge_trigger_event_listener(listener, event, C.CString(res.(string)), unsafe.Pointer(nil))
+		}
+	}()
+	(*websocket.WebSocket)(ws.instance).AddListener(int(event), channel)
+}
